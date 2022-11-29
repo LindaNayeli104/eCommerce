e2d54d6d0b27b6531ad294729c6ac81a460119d5
Linda Abundis <soporte@micronics.mx>
2022-11-29 12:53:54 -0600

    Fusion of menu in mobile

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail.addEventListener('click', toggleDesktopMenu, false);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+hamburgerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
 
